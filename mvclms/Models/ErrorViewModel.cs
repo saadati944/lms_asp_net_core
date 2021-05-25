94a96d9f1a137523624ddc090e238064d57e7cb0
Ali Saadati <saadati944@gmail.com>
2021-05-25 09:17:59 +0430

    initial commit

---
+++ mvclms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvclms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
