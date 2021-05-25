a812f79a46d14eb331fd4217537fe532794bd46e
Ali Saadati <saadati944@gmail.com>
2021-05-25 13:02:51 +0430

    create login, createuser, logout, addtorole methods to usermanager service

--- mvclms/Services/UserManager.cs
+++ mvclms/Services/UserManager.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using mvclms.Data;
 using mvclms.Models;
@@ -7,27 +8,55 @@ namespace mvclms.Services
 {
     public class UserManager
     {
-        // private readonly ApplicationDbContext _dbContext;
-        // private readonly UserManager<Teacher> _teacherUserManager;
-        // private readonly UserManager<Student> _studentUserManager;
-        // private readonly SignInManager<Teacher> _teacherSignInManager;
-        // private readonly SignInManager<Student> _studentSignInManager;
-        //
-        // public UserManager(ApplicationDbContext dbContext, UserManager<Teacher> tum, UserManager<Student> sum,
-        //     SignInManager<Teacher> tsm, SignInManager<Student> ssm)
-        // {
-        //     _dbContext = dbContext;
-        //     _teacherUserManager = tum;
-        //     _teacherSignInManager = tsm;
-        //     _studentUserManager = sum;
-        //     _studentSignInManager = ssm;
-        // }
-        //
-        //
-        // public void Login(LoginViewModel login)
-        // {
-        //     if(login.)
-        // }
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<Person> _userManager;
+        private readonly SignInManager<Person> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         
+        public UserManager(ApplicationDbContext dbContext, UserManager<Person> userManager,
+            SignInManager<Person> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+
+        public bool CreateUser(PersonViewModel person)
+        {
+            Person p = new Person
+            {
+                UserName = person.UserName,
+                FirstName = person.FirstName,
+                LastName = person.LastName
+            };
+            Task<IdentityResult> createRes =  _userManager.CreateAsync(p, person.Password);
+            createRes.Wait();
+            return createRes.Result.Succeeded && AddToRole(p, person.PersonMode);
+        }
+        
+        public bool AddToRole(Person person, string role)
+        {
+            Task<bool> res = _roleManager.RoleExistsAsync(role);
+            res.Wait();
+            if (!res.Result)
+                _roleManager.CreateAsync(new IdentityRole{Name = role}).Wait();
+            var x = _userManager.AddToRoleAsync(person, role);
+            x.Wait();
+            return x.Result.Succeeded;
+        }
+
+        public bool Login(LoginViewModel login)
+        {
+            var result = _signInManager.PasswordSignInAsync(login.Username, login.Password, login.RememberMe, false);
+            result.Wait();
+            return result.Result.Succeeded;
+        }
+
+        public void Logout()
+        {
+            _signInManager.SignOutAsync().Wait();
+        }
     }
 }
