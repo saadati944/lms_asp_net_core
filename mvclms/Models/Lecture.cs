bf01b8713a81395771b466d71a79149e85e03c56
Ali Saadati <saadati944@gmail.com>
2021-06-01 12:31:09 +0430

    add title field to lecture model

--- mvclms/Models/Lecture.cs
+++ mvclms/Models/Lecture.cs
@@ -3,6 +3,7 @@ namespace mvclms.Models
     public class Lecture
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Content { get; set; }
         public File Attachment { get; set; }
         
